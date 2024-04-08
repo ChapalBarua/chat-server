dc23105112025f0148ec7290b6e54d3bc3eb59f7
ChapalBarua <chapalbuet@yahoo.com>
2024-04-08 18:25:54 -0400

    [chat-10] fix for log

--- site.js
+++ site.js
@@ -4,6 +4,18 @@ var http = require('http');
 var https = require('https');
 const path = require('path');
 
+var fs = require('fs');
+var util = require('util');
+var logFile = fs.createWriteStream(__dirname + '/log.txt', { flags: 'a' });
+  // Or 'w' to truncate the file every time the process starts.
+var logStdout = process.stdout;
+
+console.log = function () {
+  logFile.write(util.format.apply(null, arguments) + '\n');
+  logStdout.write(util.format.apply(null, arguments) + '\n');
+}
+console.error = console.log;
+
 const app = express();
 
 // redirect https to http
