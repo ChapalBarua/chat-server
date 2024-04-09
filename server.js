e1b2d6f6251ec1e028f9f076f9610bb570af9086
ChapalBarua <chapalbuet@yahoo.com>
2024-04-08 21:28:15 -0400

    [chat-14] fix https server

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-const { WebSocketServer } = require('ws')
-const sockserver = new WebSocketServer({ port: 3000 });
-
-const https = require('https');
-const fs = require('fs');
-
+const { WebSocketServer } = require('ws');
+var https = require('https');
+var fs = require('fs');
 // var options = {
 //   key: fs.readFileSync('helpers/secrets/certs/cert.key'),
 //   cert: fs.readFileSync('helpers/secrets/certs/cert.crt')
@@ -13,19 +10,20 @@ var options = {
   key: fs.readFileSync('/home/ec2-user/secrets/certs/cert.key'),
   cert: fs.readFileSync('/home/ec2-user/secrets/certs/cert.crt')
 };
-const server = https.createServer(options, (req, res) => {
-  // Your existing HTTP server logic
-  sockserver.on('connection', ws => {
-    console.log('New client connected!');
-    ws.on('close', () => console.log('Client has disconnected!'));
-    ws.on('message', data => {
-      sockserver.clients.forEach(client => {
-        console.log(`distributing message: ${data}`);
-        client.send(`${data}`);
-      })
-    })
-    ws.onerror = function () {
-      console.log('websocket error');
-    }
+
+httpsServer = https.createServer(options).listen(3000);
+
+const sockserver = new WebSocketServer({server: httpsServer});
+sockserver.on('connection', ws => {
+ console.log('New client connected!');
+ ws.on('close', () => console.log('Client has disconnected!'));
+ ws.on('message', data => {
+   sockserver.clients.forEach(client => {
+     console.log(`distributing message: ${data}`);
+     client.send(`${data}`);
    })
-});
+ })
+ ws.onerror = function () {
+   console.log('websocket error');
+ }
+})
