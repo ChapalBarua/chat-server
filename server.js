53348f4fb8b82ee167327989de3b60ff8d320c80
ChapalBarua <chapalbuet@yahoo.com>
2024-04-08 20:49:47 -0400

    [chat-13] make server to https

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const { WebSocketServer } = require('ws')
-const sockserver = new WebSocketServer({ port: 3000 })
-sockserver.on('connection', ws => {
- console.log('New client connected!');
- ws.on('close', () => console.log('Client has disconnected!'));
- ws.on('message', data => {
-   sockserver.clients.forEach(client => {
-     console.log(`distributing message: ${data}`);
-     client.send(`${data}`);
+const sockserver = new WebSocketServer({ port: 3000 });
+
+const https = require('https');
+const fs = require('fs');
+
+// var options = {
+//   key: fs.readFileSync('helpers/secrets/certs/cert.key'),
+//   cert: fs.readFileSync('helpers/secrets/certs/cert.crt')
+// };
+
+var options = {
+  key: fs.readFileSync('/home/ec2-user/secrets/certs/cert.key'),
+  cert: fs.readFileSync('/home/ec2-user/secrets/certs/cert.crt')
+};
+const server = https.createServer(options, (req, res) => {
+  // Your existing HTTP server logic
+  sockserver.on('connection', ws => {
+    console.log('New client connected!');
+    ws.on('close', () => console.log('Client has disconnected!'));
+    ws.on('message', data => {
+      sockserver.clients.forEach(client => {
+        console.log(`distributing message: ${data}`);
+        client.send(`${data}`);
+      })
+    })
+    ws.onerror = function () {
+      console.log('websocket error');
+    }
    })
- })
- ws.onerror = function () {
-   console.log('websocket error');
- }
-})
+});
